63009f895d1532ed530e861d4fb7072c380f07ea
honda-a <arash.b7@gmail.com>
2024-09-04 15:24:21 +0900

    feat: update dependencies feat: fixed redis backend

--- fastapi_sessions/backends/implementations/redis_backend.py
+++ fastapi_sessions/backends/implementations/redis_backend.py
@@ -1,9 +1,5 @@
-from uuid import UUID, uuid4
-
-from pydantic import BaseModel
-from typing import List, Generic
+from typing import Generic, cast
 from redis.client import Redis
-import json
 
 from fastapi_sessions.backends.session_backend import ( # noqa
     BackendError,
@@ -28,27 +24,27 @@ class InRedisBackend(Generic[ID, SessionModel], SessionBackend[ID, SessionModel]
 
     async def create(self, session_id: ID, data: SessionModel):
         """Create a new session entry."""
-        if self._connect.get(str(session_id)):
+        if await self._connect.get(str(session_id)):
             raise BackendError("create can't overwrite an existing session")
 
-        self._connect.set(str(session_id), data.json())
+        await self._connect.set(str(session_id), data.model_dump_json())
 
     async def read(self, session_id: ID):
         """Read an existing session data."""
-        data = self._connect.get(str(session_id))
+        data = await self._connect.get(str(session_id))
         if not data:
             return
-
-        model = self._cls(**json.loads(data))
+        data = cast(bytes, data)
+        model = self._cls.model_validate_json(data)
         return model
 
     async def update(self, session_id: ID, data: SessionModel) -> None:
         """Update an existing session."""
-        if self._connect.get(str(session_id)):
-            self._connect.set(str(session_id), data.json())
+        if await self._connect.get(str(session_id)):
+            await self._connect.set(str(session_id), data.model_dump_json())
         else:
             raise BackendError("session does not exist, cannot update")
 
     async def delete(self, session_id: ID) -> None:
         """Delete"""
-        self._connect.delete(str(session_id))
+        await self._connect.delete(str(session_id))
